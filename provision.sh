d7d1f9916363b49c5609cf712da6758d36a40b4d
Chris Rowles <chris.rowles@godeploy.it>
2021-03-21 20:58:36 +0000

    updates

--- provision.sh
+++ provision.sh
@@ -6,17 +6,16 @@
     exit 1;
 }
 
-WD=$(pwd)
+wd=$(pwd)
 
 echo "making sure system is up-to-date"
 apt-get -y update
 apt-get -y upgrade
 
-
 # configure python3 (assumes it is installed... usually is on latest version of raspbian OS)
 echo "checking for python 3"
-PYTHONDEFAULT=`python -V | awk -F ' ' '{print $2}' | awk -F '.' '{print $1}'`;
-if [[ $PYTHONDEFAULT == "3" ]]
+python_default=`python -V | awk -F ' ' '{print $2}' | awk -F '.' '{print $1}'`;
+if [[ $python_default == "3" ]]
     then
         echo "python 3 is set as default"
     else
@@ -26,11 +25,10 @@ if [[ $PYTHONDEFAULT == "3" ]]
         update-alternatives --set python /usr/bin/python3
 fi
 
-
 # configure pip3
 echo "checking for pip3"
-PIPINSTALL=`dpkg -s python3-pip | grep Status`;
-if [[ $PIPINSTALL == S* ]]
+pip_install=`dpkg -s python3-pip | grep Status`;
+if [[ $pip_install == S* ]]
     then
         echo "pip3 is installed"
     else
@@ -40,10 +38,12 @@ fi
 echo "configuring pip3 as default"
 update-alternatives --install /usr/bin/pip pip /usr/bin/pip3 1
 update-alternatives --set pip /usr/bin/pip3
+
+# install discord.py
 echo "installing discord.py"
 pip install discord.py
 
-# make sure discord.sh is installed for webhooks.
+# install discord.sh
 echo "checking for discord.sh"
 if [ -f /usr/bin/discordnotification ];
     then
@@ -54,25 +54,31 @@ if [ -f /usr/bin/discordnotification ];
         chmod u+x /usr/bin/discordnotification
 fi
 
-
-# configure apache and virtualhost helper script
+# install apache
 echo "checking for apache"
-APACHEINSTALL=`dpkg -s apache2 | grep Status`;
-if [[ $APACHEINSTALL == S* ]]
+apache_install=`dpkg -s apache2 | grep Status`;
+if [[ $apache_install == S* ]]
     then
         echo "apache is installed"
     else
         echo "installing apache"
         apt-get -y install apache2
 fi
+
 # install mod_wsgi
 apt-get -y install libapache2-mod-wsgi-py3
+
+# start apache at boot
 echo "making sure apache is started and configured to run at boot"
 systemctl start apache2
 systemctl enable apache2
+
+# enable modules
 echo "enabling mod headers and mod rewrite"
 a2enmod headers
 a2enmod rewrite
+
+# install virtualhost helper script
 if [ -f /usr/bin/virtualhost ];
     then
         echo "virtualhost helper script is installed"
@@ -81,15 +87,16 @@ if [ -f /usr/bin/virtualhost ];
         wget https://raw.githubusercontent.com/chrisrowles/dotfiles/master/scripts/virtualhost -O /usr/bin/virtualhost
         chmod u+x /usr/bin/virtualhost
 fi
+
+# set ownership on /var/www/
 echo "adding pi to www-data group and changing ownership of /var/www"
 usermod -aG www-data pi
 chown -R pi:www-data /var/www
 
-
-# make sure fail2ban is installed
+# install fail2ban
 echo "checking for fail2ban"
-JAILINSTALL=`dpkg -s fail2ban | grep Status`;
-if [[ $JAILINSTALL == S* ]]
+fail2ban_install=`dpkg -s fail2ban | grep Status`;
+if [[ $fail2ban_install == S* ]]
     then
         echo "fail2ban is installed."
     else
@@ -103,16 +110,16 @@ if [[ $JAILINSTALL == S* ]]
         echo "copying jail configuration"
         cp $(pwd)/etc/fail2ban/jail.local /etc/fail2ban/jail.local
 fi
-# make sure fail2ban is started and enabled at boot
+
+# start fail2ban at boot
 echo "starting fail2ban and enabling at system boot"
 systemctl start fail2ban
 systemctl enable fail2ban
 
-
-# make sure git is installed
+# install git
 echo "checking for git"
-GITINSTALL=`dpkg -s git | grep Status`;
-if [[ $GITINSTALL == S* ]]
+git_install=`dpkg -s git | grep Status`;
+if [[ $git_install == S* ]]
     then
         echo "git is installed"
     else
@@ -120,8 +127,7 @@ if [[ $GITINSTALL == S* ]]
         apt-get -y install git
 fi
 
-
-# configure system monitoring api
+# install pi-monitor-api
 echo "cloning pi-monitor-api"
 if [ ! -d /var/www/flaskapps ];
     then
@@ -131,19 +137,22 @@ if [ ! -d /var/www/flaskapps ];
             rm -rf /var/www/flaskapps/pi-monitor-api
         fi
 fi
+
 git clone https://github.com/chrisrowles/pi-monitor-api.git /var/www/flaskapps/pi-monitor-api
 cd /var/www/flaskapps/pi-monitor-api
 # TODO create a virtualenv instead
 pip install testresources
 pip install -r requirements.txt
-cd $WD
+cd $wd
 chown -R pi:www-data /var/www/flaskapps
+
+# configure pi-monitor-api virtualhost
 echo "configuring virtual host"
-AVAILABLECONF=/etc/apache2/sites-available/api.raspberrypi.local.conf
-ENABLEDCONF=/etc/apache2/sites-enabled/api.raspberrypi.local.conf
-if [ -f $AVAILABLECONF ]; then
-    rm -rf $ENABLEDCONF
-    rm -rf $AVAILABLECONF
+available_conf=/etc/apache2/sites-available/api.raspberrypi.local.conf
+enabled_conf=/etc/apache2/sites-enabled/api.raspberrypi.local.conf
+if [ -f $available_conf ]; then
+    rm -rf $enabled_conf
+    rm -rf $available_conf
 fi
 if ! cat << EOF > $AVAILABLECONF
 <VirtualHost *:80>
@@ -166,11 +175,11 @@ else
     cat << EOF >> /etc/hosts
 127.0.0.1   api.raspberrypi.local
 EOF
-    ln -s $AVAILABLECONF $ENABLEDCONF
+    ln -s $available_conf $enabled_conf
     systemctl restart apache2
 fi
 
-# Copy backup scripts (assumes hdd is connected and mounted in correct location)
+# Copy backup scripts
 echo "Configuring backups"
 if [ ! -d /etc/backup ]; then
     echo "creating backup config directory"
@@ -181,28 +190,26 @@ cp $(pwd)/cron/backup-incremental-exclusions.txt /etc/backup/
 cp $(pwd)/cron/backup-incremental /etc/cron.daily/
 cp $(pwd)/cron/backup-image /etc/cron.monthly/
 
-
+# Configure discord environment variables
 echo "Please configure your discord settings."
-# Configure environment variables
-read -p "Please enter your discord user id: "  DISCORDUSERID
-read -p "Please enter your discord app token: " DISCORDBOTTOKEN
-read -p "Please enter your discord channel id for system monitoring notifications: " DISCORDCHANNELID
-read -p "Please enter your discord channel webhook url for backup job notifications: "  DISCORDBACKUPWEBHOOK
+read -p "Please enter your discord user id: "  discord_user_id
+read -p "Please enter your discord app token: " discord_app_token
+read -p "Please enter your discord channel id for system monitoring notifications: " discord_channel_id
+read -p "Please enter your discord channel webhook url for backup job notifications: "  discord_webhook
 
 cat << EOF > /etc/backup/.env
 SYSAPI_URL=http://api.raspberrypi.local/
-DISCORD_TOKEN=$DISCORDBOTTOKEN
-USER_ID=<@$DISCORDUSERID>
-CHANNEL_ID=$DISCORDCHANNELID
+DISCORD_TOKEN=$discord_app_token
+USER_ID=<@$discord_user_id>
+CHANNEL_ID=$discord_channel_id
 
-BACKUP_WEBHOOK=$DISCORDBACKUPWEBHOOK
+BACKUP_WEBHOOK=$discord_webhook
 EOF
 
-
-# Make sure supervisor is installed
+# install supervisord
 echo "checking for supervisor"
-SUPERVISORINSTALL=`dpkg -s supervisor | grep Status`;
-if [[ $SUPERVISORINSTALL == S* ]]
+supervisor_install=`dpkg -s supervisor | grep Status`;
+if [[ $supervisor_install == S* ]]
     then
         echo "supervisor is installed."
     else
@@ -210,17 +217,18 @@ if [[ $SUPERVISORINSTALL == S* ]]
         echo "Installing supervisor. Please wait..."
         apt-get -y install supervisor
 fi
-# Configure to run as user pi
+
+# Configure supervisord to run as user pi
 chown -R pi:pi /var/log/supervisor
 cp $(pwd)/etc/supervisord/supervisord.conf /etc/supervisor/supervisord.conf
 
-# Configure monitord
-echo "checking for monitord"
+# Configure pi-monitord
+echo "checking for pi-monitord"
 if [ -d /home/pi/monitord ];
     then
-        echo "monitord is installed, leaving it alone."
+        echo "pi-monitord is installed, leaving it alone."
     else
-        echo "installing monitord."
+        echo "installing pi-monitord."
         if [ ! -d /home/pi/logs ]; then
             sudo -u pi mkdir /home/pi/logs
         fi
